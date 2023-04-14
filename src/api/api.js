e0c5e44fe3a7a6b990a77ffbd131265bfb0579c7
Alfredo Ariel Diaz <alfredodiazjjy@gmail.com>
2023-04-14 16:08:23 -0300

    agrego el api.js para acceder a los datos

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+const API_VERSION_NUMBER = 1
+
+export const api = {
+  server: 'https://643955704660f26eb1b055ff.mockapi.io/', // completa con api server
+  apiVersion: `/api/v${API_VERSION_NUMBER}`
+}
+// para probar usar
+// username: user
+// password: user
---
+++ src/api/data.js
@@ -0,0 +1,92 @@
+export const data = [
+  {
+    id: '1',
+    title: 'Carnaval de los TEKIS',
+    location: 'Ciudad Cultural',
+    date: '17 al 20 de Febrero',
+    hour: '19 hs',
+    description: 'El Carnaval de los Tekis es uno de los festivales más importantes y multitudinarios de la Provincia de Jujuy. Se lleva a cabo anualmente, durante el mes de febrero, en el Predio Ciudad Cultural de San Salvador de Jujuy',
+    images: [
+      require('../../assets/images/carnaval-tekis-1.jpg'),
+      require('../../assets/images/carnaval-tekis-2.jpg'),
+      require('../../assets/images/carnaval-tekis-3.jpg')
+    ],
+    locationCoordinates: {
+      latitude: -24.1833665,
+      longitude: -65.3312861
+    },
+    url: 'https://festivalear.com/es/eventos/carnaval-de-los-tekis'
+  },
+  {
+    id: '2',
+    title: 'Virgen de Copacabana de Punta Corral',
+    location: 'Tumbaya, Jujuy',
+    date: '24 de marzo al 29 de abril',
+    hour: '19 hs',
+    description: 'La historia de la aparición de la imagen de la Virgen de Punta Corral nace en la primera década del siglo XIX. El 22 de julio de 1835 Don Pablo Méndez, nativo del lugar, se encontraba cuidando a sus animales cerca del Abra de Estancia Vieja donde tuvo una visión sobrenatural',
+    images: [
+      require('../../assets/images/punta-corral-1.jpg'),
+      require('../../assets/images/punta-corral-2.jpg'),
+      require('../../assets/images/punta-corral-3.jpg')
+    ],
+    locationCoordinates: {
+      latitude: -23.8576330,
+      longitude: -65.4676103
+    },
+    url: 'https://www.somosjujuy.com.ar/jujuy/historia-virgen-punta-corral-jujuy-n71147#:~:text=La%20devoci%C3%B3n%20a%20la%20Virgen,visi%C3%B3n%2C%20ah%C3%AD%20comenz%C3%B3%20la%20historia%20'
+  },
+  {
+    id: '3',
+    title: 'Gran Peña Día del Trabajador',
+    location: 'Finca La Herminia - RP. N° 35 km 3',
+    data: '30 de Abril',
+    hour: '19 hs',
+    description: 'Alejandro Araujo, integrante de los Changos de Nieva y uno de los organizadores de la Gran Peña “Día del Trabajador” en su visita a Radio Universidad invitó a este evento que se realizará el lunes 30 de abril a partir de las 22 horas en Finca la Herminia. Parte de lo recaudado será entregado el 25 de mayo a la Escuela  44 “Jose Ignacio Gorriti , escuela Albergue de León.',
+    images: [
+      require('../../assets/images/peña-dia-trabajador-1.jpg'),
+      require('../../assets/images/peña-dia-trabajador-2.jpg'),
+      require('../../assets/images/peña-dia-trabajador-3.jpg')
+    ],
+    locationCoordinates: {
+      latitude: -24.1669208,
+      longitude: -65.2958169
+    },
+    url: 'http://www.turismo.jujuy.gob.ar/event/gran-pena-dia-del-trabajador/'
+  },
+  {
+    id: '4',
+    title: 'After Office Experiencia de Cliente',
+    location: 'MINTA Point 1079 Salta San Salvador de Jujuy',
+    data: '20 de Abril',
+    hour: '19:30 a 20:30',
+    description: '¡Primer After Office del año! Nos enfocamos en la Experiencia de Cliente (CX), la clave para destacar en un mercado cada vez más competitivo. La CX se trata de crear una experiencia única y satisfactoria para el cliente, que va más allá de simplemente vender un producto o servicio',
+    images: [
+      require('../../assets/images/after-office-1.jpg'),
+      require('../../assets/images/after-office-2.jpg'),
+      require('../../assets/images/after-office-3.jpg')
+    ],
+    locationCoordinates: {
+      latitude: -24.1815301,
+      longitude: -65.3082825
+    },
+    url: 'https://www.eventbrite.com.ar/e/after-office-experiencia-de-cliente-tickets-586060812667?aff=ebdssbdestsearch'
+  },
+  {
+    id: '5',
+    title: 'Fiesta Nacional de los Estudiantes',
+    location: 'San Salvador de Jujuy',
+    date: '20 al 30 de Septiembre',
+    hour: '19 hs',
+    description: 'La Fiesta Nacional de los Estudiantes (FNE) es una festividad originaria de San Salvador de Jujuy que se celebra con carácter anual en distintos puntos de la Provincia de Jujuy, Argentina. La Fiesta tiene lugar durante la Semana del Estudiante argentina y da la bienvenida a la primavera',
+    images: [
+      require('../../assets/images/fiesta-nacional-de-los-estudiantes-1.jpg'),
+      require('../../assets/images/fiesta-nacional-de-los-estudiantes-2.jpg'),
+      require('../../assets/images/fiesta-nacional-de-los-estudiantes-3.jpg')
+    ],
+    locationCoordinates: {
+      latitude: -24.1833665,
+      longitude: -65.3312861
+    },
+    url: 'https://www.facebook.com/FiestaNacionalDeLosEstudiantesOficial/?locale=es_LA'
+  }
+]
